9fc07e96d9951e852a4a0494dabc1c4c44e81aac
sergey <sergeypalii@gmail.com>
2024-04-09 14:55:33 +0500

    create user

--- HW_4.go
+++ HW_4.go
@@ -1,15 +1,68 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+	"net/http"
+)
 
-type service struct {
-	store map[string]string
+type User struct {
+	ID       string   `json:"id"`
+	UserName string   `json:"username"`
+	Age      int      `json:"age"`
+	Friends  []string `json:"friends"`
 }
 
+var users = make(map[string]User) // Переменная для хранения пользователей
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("I am alive!"))
-	})
-	http.ListenAndServe(":8080", mux)
+	r := chi.NewRouter()
+	r.Post("/create", CreateUser)
+	r.Get("/user/{userID}", GetUser)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := generateUserID() // Генерация уникального ID для пользователя
+	newUser.ID = id
+	users[id] = newUser // Сохранение пользователя в мапе
+
+	jsonResponse := map[string]string{"id": id}
+	response, err := json.Marshal(jsonResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
+
+func generateUserID() string {
+	id := uuid.New().String()
+	return id
+}
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	// Получаем уникальный идентификатор пользователя из URL
+	userID := chi.URLParam(r, "userID")
+
+	// Получаем данные пользователя из вашего хранилища
+	user, ok := users[userID]
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	// Преобразуем данные пользователя в формат JSON и отправляем их клиенту
+	json.NewEncoder(w).Encode(user)
 }
